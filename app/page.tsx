3d51a76b598c73a9ca17bd36c682a96a60a2c257
lilKriT <lilKriTwebDev@gmail.com>
2026-02-27 00:33:41 +0100

    Added new files.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      {/* Sign-up form start */}
+
+      <p>Stay updated!</p>
+
+      <p>Join 60,000+ product managers receiving monthly updates on:</p>
+
+      <p>
+        Product discovery and building what matters Measuring to ensure updates
+        are a success And much more!
+      </p>
+
+      <p>Email address email@company.com</p>
+
+      <p>Subscribe to monthly newsletter</p>
+
+      {/* Sign-up form end */}
+
+      {/* Success message start */}
+
+      <p>Thanks for subscribing!</p>
+
+      <p>
+        A confirmation email has been sent to ash@loremcompany.com. Please open
+        it and click the button inside to confirm your subscription.
+      </p>
+
+      <p>Dismiss message</p>
+
+      {/* Success message end */}
+    </div>
+  );
 }
